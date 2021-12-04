415a2f5dcec94ccf9286406fbbfca6ce3c55b91e
Dominika Piechota <piechota.domi@gmail.com>
2021-12-04 22:43:50 +0100

    code cleanup

--- src/main/java/com/example/joboffer/domain/JobOfferService.java
+++ src/main/java/com/example/joboffer/domain/JobOfferService.java
@@ -12,9 +12,6 @@ public class JobOfferService {
     public JobOfferService(JobOfferRepository jobOfferRepository) {
         this.jobOfferRepository = jobOfferRepository;
     }
-    /*public void saveJobOffer(JobOfferDTO jobOfferDTO) {
-        return new JobOffer();
-    }*/
 
     //TODO: Use JpaSpecificationExecutor
     public List<JobOffer> getValidOffers(UserName name, Category category) {
@@ -28,9 +25,4 @@ public class JobOfferService {
         }
         return jobOfferRepository.findAllBetweenStartDateAndEndDate(today);
     }
-
-    /*public List<JobOffer> getValidOffersByCategory(Category category) {
-        LocalDate today = LocalDate.now();
-        return jobOfferRepository.findAllByStartDateBeforeAndEndDateAfterAndCategory(today, category);
-    }*/
 }
