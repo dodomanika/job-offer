931d74c10d4458c57aaedb66e9e20e50153ec741
Dominika Piechota <piechota.domi@gmail.com>
2021-12-04 21:48:52 +0100

    add lombok dependency and user name fields

--- src/main/java/com/example/joboffer/domain/UserName.java
+++ src/main/java/com/example/joboffer/domain/UserName.java
@@ -1,7 +1,21 @@
 package com.example.joboffer.domain;
 
+import lombok.Getter;
+
 import javax.persistence.Embeddable;
 
 @Embeddable
+@Getter
+//TODO: Validate input
+//TODO: Add tests
 public class UserName {
+    private String name;
+
+    public UserName(String name) {
+        this.name = name;
+    }
+
+    /*For JPA only*/
+    protected UserName() {
+    }
 }
