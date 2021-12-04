d9ac67814ec521a494de4a537c14ddd278ff8657
Dominika Piechota <piechota.domi@gmail.com>
2021-12-04 21:49:57 +0100

    add initial data when starting app

---
+++ src/main/java/com/example/joboffer/InitialRunner.java
@@ -0,0 +1,32 @@
+package com.example.joboffer;
+
+import com.example.joboffer.domain.Category;
+import com.example.joboffer.domain.JobOffer;
+import com.example.joboffer.domain.JobOfferRepository;
+import com.example.joboffer.domain.UserName;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class InitialRunner implements ApplicationRunner {
+
+    private final JobOfferRepository jobOfferRepository;
+
+    public InitialRunner(JobOfferRepository jobOfferRepository) {
+        this.jobOfferRepository = jobOfferRepository;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        List<JobOffer> offers = new ArrayList<>();
+        offers.add(new JobOffer(Category.IT, LocalDate.now(), LocalDate.now().plusDays(5), new UserName("Mary")));
+        offers.add(new JobOffer(Category.SHOP_ASSISTANT, LocalDate.now(), LocalDate.now().plusDays(5), new UserName("John")));
+
+        jobOfferRepository.saveAll(offers);
+    }
+}
